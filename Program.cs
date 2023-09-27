acbe0dd921a1fd264af82e6e8098c48ac87537ad
lina-love <144587988+lina-love@users.noreply.github.com>
2023-09-27 10:34:34 +0000

    added a comment

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿
 Console.WriteLine("Benchmark Started");
 
+// The following line is for Benchmark to Start.
 BenchmarkRunner.Run<ProductsCreationBenchmark>();
 
 Console.WriteLine("Benchmark Finished");
